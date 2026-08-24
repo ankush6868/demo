cb57282e97d66b63b118f0ca911e3452e75c2775
AnkushYadav <112002ankush@gmail.com>
2026-08-24 15:48:49 +0000

    Added  changes to hello

--- scripts/hello_ankush.sh
+++ scripts/hello_ankush.sh
@@ -3,7 +3,7 @@ echo "Hello I am Ankush Yadav and I larn Devops tools"
 echo "Now I a completed in Dvops toopls in 6 month "
 echo " So , Let's start "
 echo "To the Shell Scriprting today "
-
+echo "we are larning git"
 echo "lets make a folder "
 mkdir -p rancho
 echo "Done"
